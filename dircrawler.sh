33b0b1920a17312c73358f76bc89c50a88d3def9
just1nn1t <118996565+just1nn1t@users.noreply.github.com>
2023-11-03 17:36:14 +0100

    dircrawler.sh

--- dircrawler.sh
+++ dircrawler.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-scan_directory() {
-  base_url="$1"
+scandir() {
+  baseurl="$1"
   wordlist="$2"
 
   # Loop through dirs from wordlist
   while IFS= read -r directory; do
-    url="$base_url/$directory"
-    response_code=$(curl -s -o /dev/null -w "%{http_code}" "$url")
+    url="$baseurl/$directory"
+    response=$(curl -s -o /dev/null -w "%{http_code}" "$url")
 
     # Check if the HTTP response code is 200 (OK)
-    if [ "$response_code" -eq 200 ]; then
+    if [ "$response" -eq 200 ]; then
       echo "Directory found: $url"
     fi
   done < "$wordlist"
@@ -20,24 +20,24 @@ main() {
   if [ "$#" -ne 2 ]; then
     echo "Usage: $0 <base_url> <wordlist_file>"
     exit 1
-  }
+  fi
 
-  base_url="$1"
+  baseurl="$1"
   wordlist="$2"
 
   # Check if the wordlist exists
   if [ ! -f "$wordlist" ]; then
     echo "Wordlist file not found: $wordlist"
     exit 1
-  }
+  fi
 
   # Check if the 'curl' command is available
   if ! command -v curl &> /dev/null; then
     echo "curl is required but not installed. Please install curl."
     exit 1
-  }
+  fi
 
-  scan_directory "$base_url" "$wordlist"
+  scandir "$baseurl" "$wordlist"
 }
 
 main "$@"
